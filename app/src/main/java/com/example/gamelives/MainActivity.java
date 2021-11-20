b525d411092683307905276513ea49d1af16195c
AGranjaL <aaron.granja.l@gmail.com>
2021-11-20 14:14:52 +0100

    added second layout

--- app/src/main/java/com/example/gamelives/MainActivity.java
+++ app/src/main/java/com/example/gamelives/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.gamelives;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -22,9 +24,10 @@ public class MainActivity extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 System.out.println("Button pressed");
+
+                // Code here executes on main thread after user presses button
+                setContentView(R.layout.activity_second);
             }
-            // Code here executes on main thread after user presses button
-            
         });
 
 
---
+++ app/src/main/java/com/example/gamelives/SecondActivity.java
@@ -0,0 +1,11 @@
+package com.example.gamelives;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SecondActivity extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+    }
+}
