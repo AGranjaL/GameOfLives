b5df761cbce64da9613ced3c8e770411ae7ef91f
fuematthias <81610638+fuematthias@users.noreply.github.com>
2021-12-09 20:06:06 +0100

    worked on activity listener

--- app/src/main/java/com/example/gamelives/BidActivity.java
+++ app/src/main/java/com/example/gamelives/BidActivity.java
@@ -3,6 +3,8 @@ package com.example.gamelives;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.widget.NumberPicker;
+import android.widget.Toast;
 
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,16 +14,37 @@ public class BidActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.bid_layout);
-        Handler handler = new Handler();
-        handler.postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                System.out.println("HOLA");
-            }
-        }, 4000);
-        Intent second_act = new Intent(BidActivity.this, SecondActivity.class);
-        startActivity(second_act);
-        setContentView(R.layout.activity_second);
 
+        NumberPicker np = findViewById(R.id.bid);
+
+        np.setMinValue(0);
+        np.setMinValue(20);
+
+        np.setOnValueChangedListener(onValueChangeListener);
     }
+
+    NumberPicker.OnValueChangeListener onValueChangeListener = new NumberPicker.OnValueChangeListener() {
+        @Override
+        public void onValueChange(NumberPicker numberPicker, int i, int i1) {
+            Toast.makeText(BidActivity.this, "selected number" +bid.getValue(), Toast.LENGTH_SHORT);
+        }
+    };
+/*
+    Handler handler = new Handler();
+        handler.postDelayed(new
+
+    Runnable() {
+        @Override
+        public void run () {
+            System.out.println("HOLA");
+        }
+    },4000);
+    Intent second_act = new Intent(BidActivity.this, SecondActivity.class);
+
+    startActivity(second_act);
+
+    setContentView(R.layout.activity_second);
+
+
+ */
 }
